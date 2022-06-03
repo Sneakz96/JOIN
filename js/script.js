bbd17c4a7c0a54c8ec902c68d519804fbfbd2146
Jahleel <Jahleelr-h@gmx.de>
2022-06-04 00:35:50 +0200

    add templates and clean up code

--- js/script.js
+++ js/script.js
@@ -17,27 +17,9 @@ function createTask() {
     let toDo = document.getElementById('toDo');
     let logs = document.getElementById('logs');
 
-    toDo.innerHTML += /*html*/ `<div class="task bg-blue" id="">
-    <span>Viel Spaß! Die Daten sind am Server!</span>
-    </div>`
+    toDo.innerHTML += createTask_TEMPLATE_TO_DO();
 
-    logs.innerHTML += /*html*/ `<div class="contact-card">
-    <div class="color"></div>
-    <div class="infos">
-        <div class="contact">
-            <img src="./img/jahleel.jpg" class="profile-picture img">
-            <div class="contact-name">
-                <span>Jahleel Heider</span>
-                <a href="#">Jahleelr-h@gmx.de</a>
-            </div>
-        </div>
-        <div id="category">
-            Office
-        </div>
-        <div class="detail-card">Viel Spass! Die Daten sind am Server! :)</div>
-        <img src="./icons/info-2-32.png" class="info-icon">
-    </div>
-</div>`
+    logs.innerHTML += createTask_TEMPLATE_LOGS();
 }
 
 
---
+++ js/template.js
@@ -0,0 +1,26 @@
+function createTask_TEMPLATE_TO_DO(){
+return /*html*/ `<div class="task bg-blue" id="">
+<span>Viel Spaß! Die Daten sind am Server!</span>
+</div>`
+}
+
+
+function createTask_TEMPLATE_LOGS(){
+    return /*html*/ `<div class="contact-card">
+    <div class="color"></div>
+    <div class="infos">
+        <div class="contact">
+            <img src="./img/jahleel.jpg" class="profile-picture img">
+            <div class="contact-name">
+                <span>Jahleel Heider</span>
+                <a href="#">Jahleelr-h@gmx.de</a>
+            </div>
+        </div>
+        <div id="category">
+            Office
+        </div>
+        <div class="detail-card">Viel Spass! Die Daten sind am Server! :)</div>
+        <img src="./icons/info-2-32.png" class="info-icon">
+    </div>
+</div>`;
+}
