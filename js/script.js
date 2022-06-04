8ca7b2f5e1db6585d4011a6a9cfd99e8d77e0f60
Jahleel <Jahleelr-h@gmx.de>
2022-06-04 16:50:00 +0200

    clean up code & add process

--- js/script.js
+++ js/script.js
@@ -1,150 +1,58 @@
 let allTasks = [];
 
-
+//FUNCTION_AT_START
 function init() {
-
+    loadAllTasks();
 }
 
+//FUNCTION_LOAD_EXISTING_TASKS
+function loadAllTasks(){
+    let allTasksAsString = localStorage.getItem('allTasks');
+    allTasks = JSON.parse(allTasksAsString);
+    console.log('loaded tasks', allTasks)
+}
 
+//FUNCTION_CREATE_TASK
 function createTask() {
     let title = document.getElementById('title').value;
     let category = document.getElementById('categoryBtn').innerHTML;
     let description = document.getElementById('description').value;
     let dueDate = document.getElementById('dueDate').value;
-    let urgency = document.getElementById('urgencyBtn').innerHTML;
+    let urgency = document.getElementById('urgencyBtn').value;
     //let asiTo = document.getElementById('').value;
 
     let task = {
-        'title' :title,
-        'category' : category,
-        'description' : description,
-        'createdAt' : new Date().getTime(),
+        'title': title,
+        'category': category,
+        'description': description,
+        'createdAt': new Date().getTime(),
         'dueDate': dueDate,
-        'urgency' : urgency,
+        'urgency': urgency,
+        'assigned to': 'me',
     }
-    
-    
+
     allTasks.push(task);
-    
-    console.log(allTasks);
-
-
+    let allTasksAsString = JSON.stringify(allTasks);
+    localStorage.setItem('allTasks', allTasksAsString);
     addToDo();
     addBacklog();
-
 }
 
-
-
-
+//FUNCTION_ADD_TO_TO_DO
 function addToDo() {
     let toDo = document.getElementById('toDo');
     toDo.innerHTML += createTask_TEMPLATE_TO_DO();
 }
 
+//FUNCTION_ADD_TO_BACKLOG
 function addBacklog() {
     let logs = document.getElementById('logs');
     logs.innerHTML += createTask_TEMPLATE_LOGS();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*CHANGE CATEGORYBUTTON.INNER HTML TO MANAGEMENT*/
-function changeCategoryButtonTxt_MANAGEMENT() {
-    let categoryBtn = document.getElementById('categoryBtn');
-    categoryBtn.innerHTML = 'Management';
-}
-
-/*CHANGE CATEGORYBUTTON.INNER HTML TO MARKETING*/
-function changeCategoryButtonTxt_MARKETING() {
-    let categoryBtn = document.getElementById('categoryBtn');
-    categoryBtn.innerHTML = 'Marketing';
-}
-
-/*CHANGE CATEGORYBUTTON.INNER HTML TO FINANCE*/
-function changeCategoryButtonTxt_FINANCE() {
-    let categoryBtn = document.getElementById('categoryBtn');
-    categoryBtn.innerHTML = 'Finance';
-}
-
-/*CHANGE CATEGORYBUTTON.INNER HTML TO OPERATION MANAGEMENT*/
-function changeCategoryButtonTxt_OPERATION_MANAGEMENT() {
-    let categoryBtn = document.getElementById('categoryBtn');
-    categoryBtn.innerHTML = 'Operation Management';
-}
-
-/*CHANGE CATEGORYBUTTON.INNER HTML TO IT*/
-function changeCategoryButtonTxt_IT() {
-    let categoryBtn = document.getElementById('categoryBtn');
-    categoryBtn.innerHTML = 'IT';
-}
-
-/*CHANGE CATEGORYBUTTON.INNER HTML TO HUMAN RESOURCE*/
-function changeCategoryButtonTxt_HUMAN_RESOURCE() {
-    let categoryBtn = document.getElementById('categoryBtn');
-    categoryBtn.innerHTML = 'Human Resource';
-}
-
-/*CHANGE CATEGORYBUTTON.INNER HTML TO COSTUMER SERVICE*/
-function changeCategoryButtonTxt_COSTUMER_SERVICE() {
-    let categoryBtn = document.getElementById('categoryBtn');
-    categoryBtn.innerHTML = 'Costumer Service';
-}
-
-/*CHANGE CATEGORYBUTTON.INNER HTML TO PRODUCTION*/
-function changeCategoryButtonTxt_PRODUCTION() {
-    let categoryBtn = document.getElementById('categoryBtn');
-    categoryBtn.innerHTML = 'Production';
-}
-
-/*CHANGE CATEGORYBUTTON.INNER HTML TO TEAM*/
-function changeCategoryButtonTxt_TEAM() {
-    let categoryBtn = document.getElementById('categoryBtn');
-    categoryBtn.innerHTML = 'Team';
-}
-
-
-
-
-
-
-
-
-
-/*CHANGE URGENCYBUTTON.INNER HTML TO LOW*/
-function changeUrgencyButtonTxt_LOW() {
-    let urgencyBtn = document.getElementById('urgencyBtn');
-    urgencyBtn.innerHTML = 'Low';
-}
-
-/*CHANGE URGENCYBUTTON.INNER HTML TO MID*/
-function changeUrgencyButtonTxt_MID() {
-    let urgencyBtn = document.getElementById('urgencyBtn');
-    urgencyBtn.innerHTML = 'Mid';
-}
-
-/*CHANGE URGENCYBUTTON.INNER HTML TO HIGH*/
-function changeUrgencyButtonTxt_HIGH() {
-    let urgencyBtn = document.getElementById('urgencyBtn');
-    urgencyBtn.innerHTML = 'High';
+//FUNCTION_
+function openTeam(){
+    let team = document.getElementById('team');
+    team.classList.remove('d-none');
+    team.innerHTML = prompt('show team');
 }
